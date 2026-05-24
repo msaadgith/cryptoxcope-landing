5211852051a35c00178680d55879ed46c997cd83
msaadgith <msaadmadeira@gmail.com>
2026-05-24 14:37:02 -0300

    version for page views

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'CryptoXcope',
@@ -13,7 +14,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className="
+          bg-[#050816]
+          text-white
+        "
+      >
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   );
 }
